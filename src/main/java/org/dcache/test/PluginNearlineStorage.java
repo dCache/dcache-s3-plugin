06fb9eda8ffc88aa407bd63b3156392c76155de6
Svenja Meyer <svenja.meyer@desy.de>
2020-03-27 13:49:48 +0100

    Make plugin work

--- src/main/java/org/dcache/test/PluginNearlineStorage.java
+++ src/main/java/org/dcache/test/PluginNearlineStorage.java
@@ -1,6 +1,8 @@
 package org.dcache.test;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
@@ -11,6 +13,7 @@ import java.util.concurrent.*;
 
 import io.minio.MinioClient;
 import io.minio.errors.*;
+import org.apache.commons.io.FileUtils;
 import org.dcache.pool.nearline.spi.FlushRequest;
 import org.dcache.pool.nearline.spi.NearlineStorage;
 import org.dcache.pool.nearline.spi.RemoveRequest;
@@ -22,25 +25,17 @@ public class PluginNearlineStorage implements NearlineStorage
     protected final String type;
     protected final String name;
 
-    private final MinioClient minio;
+    String endpoint = "";
+    String accessKey = "";
+    String secretKey = "";
+
+    private MinioClient minio;
     private final ExecutorService executor = Executors.newFixedThreadPool(3);
     private List<FutureTask> taskList = new ArrayList<FutureTask>();
 
     public PluginNearlineStorage(String type, String name) {
         this.type = type;
         this.name = name;
-        try {
-            String endpoint = "";
-            String accessKey = "";
-            String secretKey = "";
-            minio = new MinioClient(endpoint, accessKey, secretKey);
-        } catch (InvalidEndpointException | InvalidPortException iee) {
-            System.out.println("Exception creating minio client: " + iee);
-            throw new RuntimeException("Unable to create Minio client");
-        } catch (Exception e) {
-            System.out.println("Unknown error: " + e);
-            throw new RuntimeException("Unable to create Minio client");
-        }
     }
 
     /**
@@ -53,55 +48,38 @@ public class PluginNearlineStorage implements NearlineStorage
     {
         System.out.println("Flush triggered");
 
-        for (FlushRequest fRequest: requests
-             ) {
+        for (FlushRequest fRequest: requests) {
             FutureTask<UUID> flushTask = new FutureTask<UUID>(new Callable() {
                 @Override
-                public UUID call() throws Exception {
+                public UUID call() {
                     System.out.println("Flush file " + fRequest.getReplicaUri().getPath() + " -- " + fRequest.getId());
 
                     String bucketName = fRequest.getFileAttributes().getStorageClass().toLowerCase()
                             .replaceAll("[^a-z-.]", ".");
                     String pnfsId = fRequest.getFileAttributes().getPnfsId().toString();
                     String source = fRequest.getReplicaUri().getPath();
+
                     fRequest.activate();
+
                     try {
                         boolean bucketExists = minio.bucketExists(bucketName);
                         if (!bucketExists) {
                             minio.makeBucket(bucketName);
                         }
-                    } catch (InvalidBucketNameException | NoSuchAlgorithmException | InsufficientDataException |
-                            IOException | InvalidKeyException | NoResponseException | XmlPullParserException |
-                            ErrorResponseException | InternalException | InvalidResponseException |
-                            RegionConflictException e) {
+                        minio.putObject(bucketName, pnfsId, source, fRequest.getFileAttributes().getSize(),
+                                null, null, null);
+                        fRequest.completed(Collections.singleton(new URI(type, name, '/' +
+                                fRequest.getFileAttributes().getPnfsId().toString(), null, null)));
+                    } catch (InvalidBucketNameException | NoSuchAlgorithmException | InsufficientDataException | IOException | InvalidKeyException | NoResponseException | XmlPullParserException | ErrorResponseException | InternalException | InvalidResponseException | RegionConflictException | InvalidArgumentException | URISyntaxException e) {
                         fRequest.failed(e);
                         System.out.println("Flush " + pnfsId + " failed, error with bucket exists and creation: " + e);
                         return null;
                     }
-                    try {
-                        minio.putObject(bucketName, pnfsId, source, fRequest.getFileAttributes().getSize(),
-                                null, null, null);
-                    } catch (InvalidKeyException | InsufficientDataException | NoSuchAlgorithmException |
-                            NoResponseException | InvalidResponseException | XmlPullParserException |
-                            InvalidArgumentException | InvalidBucketNameException | ErrorResponseException |
-                            InternalException | IOException e) {
-                        fRequest.failed(e);
-                        System.out.println("Writing file " + pnfsId + " to S3 tape failed: " + e);
-                        return null;
-                    }
-                    try {
-                        fRequest.completed(Collections.singleton(new URI(type, name, '/' +
-                                fRequest.getFileAttributes().getPnfsId().toString(), null, null)));
-                    } catch (URISyntaxException use) {
-                        fRequest.failed(use);
-                        System.out.println("Marking Request as completed failed, caused by URI: " + use);
-                    }
-                    System.out.println("File " + source + " flushed to tape with pnfsID " + pnfsId);
                     return fRequest.getId();
                 }
             });
             taskList.add(flushTask);
-            executor.execute(flushTask);
+            CompletableFuture.runAsync(flushTask, executor);
         }
 
     }
@@ -118,25 +96,37 @@ public class PluginNearlineStorage implements NearlineStorage
 
         for (final StageRequest sRequest : requests
              ) {
-            executor.execute(() -> {
-                sRequest.activate();
-                sRequest.allocate();
-                String bucketName = sRequest.getFileAttributes().getStorageClass().toLowerCase()
-                        .replaceAll("[^a-z-.]", ".");
-                String objectName = sRequest.getFileAttributes().getPnfsId().toString();
-                try {
+            FutureTask<UUID> stageTask = new FutureTask<UUID>(new Callable() {
+                @Override
+                public UUID call() {
+                    sRequest.activate();
+                    sRequest.allocate();
 
-                    minio.getObject(bucketName, objectName, "test123.txt");
-                    sRequest.completed(sRequest.getFileAttributes().getChecksumsIfPresent().isPresent() ?
-                            sRequest.getFileAttributes().getChecksums() : null);
-                } catch (InvalidKeyException | NoSuchAlgorithmException | NoResponseException |
-                        InvalidResponseException | XmlPullParserException | InvalidBucketNameException |
-                        InvalidArgumentException | InsufficientDataException | ErrorResponseException |
-                        InternalException | IOException e) {
-                    sRequest.failed(e);
-                    System.out.println("Stage request failed: " + e);
+                    URI destination = sRequest.getReplicaUri();
+                    String bucketName = sRequest.getFileAttributes().getStorageClass().toLowerCase()
+                            .replaceAll("[^a-z-.]", ".");
+                    String objectName = sRequest.getFileAttributes().getPnfsId().toString();
+                    try {
+
+                        InputStream content = minio.getObject(bucketName, objectName);
+                        File target = new File(destination);
+                        FileUtils.copyInputStreamToFile(content, target);
+                        System.out.println("File erstellt: " + destination);
+                        sRequest.completed(sRequest.getFileAttributes().getChecksumsIfPresent().isPresent() ?
+                                sRequest.getFileAttributes().getChecksums() : null);
+                    } catch (InvalidKeyException | NoSuchAlgorithmException | NoResponseException |
+                            InvalidResponseException | XmlPullParserException | InvalidBucketNameException |
+                            InvalidArgumentException | InsufficientDataException | ErrorResponseException |
+                            InternalException | IOException e) {
+                        sRequest.failed(e);
+                        System.out.println("Stage request failed: " + e);
+                        return null;
+                    }
+                    return sRequest.getId();
                 }
             });
+            taskList.add(stageTask);
+            CompletableFuture.runAsync(stageTask, executor);
         }
     }
 
@@ -153,9 +143,9 @@ public class PluginNearlineStorage implements NearlineStorage
         for (RemoveRequest rRequest: requests
              ) {
 
-            FutureTask ft = new FutureTask(new Callable() {
+            FutureTask<UUID> removeTask = new FutureTask<UUID>(new Callable() {
                 @Override
-                public Object call() throws Exception {
+                public UUID call() {
                     System.out.println("Remove " + rRequest.getUri().getPath().replace("/", ""));
                     rRequest.activate();
                     try {
@@ -173,8 +163,8 @@ public class PluginNearlineStorage implements NearlineStorage
                     }
                 }
             });
-            executor.execute(ft);
-            taskList.add(ft);
+            executor.execute(removeTask);
+            CompletableFuture.runAsync(removeTask, executor);
         }
     }
 
@@ -219,6 +209,20 @@ public class PluginNearlineStorage implements NearlineStorage
     public void configure(Map<String, String> properties) throws IllegalArgumentException
     {
         System.out.println("Configure triggered");
+        endpoint = properties.getOrDefault("endpoint", "");
+        accessKey = properties.getOrDefault("access_key", "");
+        secretKey = properties.getOrDefault("secret_key", "");
+
+        try {
+            minio = new MinioClient(endpoint, accessKey, secretKey);
+        } catch (InvalidEndpointException | InvalidPortException iee) {
+            System.out.println("Exception creating minio client: " + iee);
+            throw new RuntimeException("Unable to create Minio client");
+        } catch (Exception e) {
+            System.out.println("Unknown error: " + e);
+            throw new RuntimeException("Unable to create Minio client");
+        }
+
     }
 
     /**
